56cd5807046f21724addbd0b0ef960f8bb7c8486
Michael <stevenex13@gmail.com>
2025-06-07 12:59:06 -0500

    permitir a todos los hosts

--- backend/settings.py
+++ backend/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.onrender.com').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 
 
