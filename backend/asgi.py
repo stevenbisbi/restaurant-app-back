a57eabdc89dc0bbd0a44597fc19b402ebdc89d00
Michael <stevenex13@gmail.com>
2025-06-07 12:52:35 -0500

    cabio asgi 3

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,13 +8,18 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
 from channels.auth import AuthMiddlewareStack # type: ignore
 from django.conf import settings
-from orders.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+
+# Inicializa Django antes de importar cualquier modelo
+django.setup()
+
+from orders.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
